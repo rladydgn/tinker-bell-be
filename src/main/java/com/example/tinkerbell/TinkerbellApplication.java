4d2624da6fec1350f3385d761a8199b26a3b82c8
rladydgn <39542757+rladydgn@users.noreply.github.com>
2024-07-06 22:43:58 +0900

    feat: 프로젝트 init

---
+++ src/main/java/com/example/tinkerbell/TinkerbellApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinkerbell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkerbellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkerbellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tinkerbell/TinkerbellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinkerbell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkerbellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
