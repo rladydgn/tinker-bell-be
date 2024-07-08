0962143bb8f4c8cb831766bae6ae8167a69b6ddd
rladydgn <39542757+rladydgn@users.noreply.github.com>
2024-07-09 01:46:50 +0900

    fix: 오류 수정

--- src/main/java/com/example/tinkerbell/HelloController.java
+++ src/main/java/com/example/tinkerbell/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.tinkerbell;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
+
+    @Value("{hello}")
+    private String he;
 
     @GetMapping
     public String hello() {
+        System.out.println(he);
         return "hello";
     }
 }
