ccf2b0b343e75cdd5ccebaa4ebff8f64b6192e3e
rladydgn <dhdgn@naver.com>
2024-09-05 00:10:44 +0900

    로컬 쿠키 설정 수정

--- src/main/java/com/example/tinkerbell/oAuth/controller/OAuthController.java
+++ src/main/java/com/example/tinkerbell/oAuth/controller/OAuthController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.tinkerbell.oAuth.dto.TokenDto;
 import com.example.tinkerbell.oAuth.service.OAuthService;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -38,24 +38,47 @@ public class OAuthController {
 			redirectUrl = feUrl;
 		}
 
-		Cookie accessTokenCookie = new Cookie("accessToken", tokenDto.getAccessToken());
-		accessTokenCookie.setPath("/");
-
-		Cookie refreshTokenCookie = new Cookie("refreshToken", tokenDto.getRefreshToken());
-		refreshTokenCookie.setPath("/");
-
 		// 쿠키 허용 도메인
 		URI uri = new URI(redirectUrl);
 		String domain = uri.getHost();
-		if (domain.contains("www")) {
+		// 서버 호출시
+		if(domain.contains("www")) {
 			domain = domain.replace("www", "");
-			accessTokenCookie.setDomain(domain);
-			refreshTokenCookie.setDomain(domain);
 			log.info("check: " + domain);
+
+			String accessTokenCookie = ResponseCookie.from("accessToken", tokenDto.getAccessToken())
+				.domain(domain)
+				.path("/")
+				.sameSite("lax")
+				.build()
+				.toString();
+
+			String refreshTokenCookie = ResponseCookie.from("refreshToken", tokenDto.getRefreshToken())
+				.domain(domain)
+				.path("/")
+				.sameSite("lax")
+				.build()
+				.toString();
+
+			response.addHeader("set-Cookie", accessTokenCookie);
+			response.addHeader("set-Cookie", refreshTokenCookie);
+		} else { // 로컬 호출시
+			String accessTokenCookie = ResponseCookie.from("accessToken", tokenDto.getAccessToken())
+				.path("/")
+				.sameSite("lax")
+				.build()
+				.toString();
+
+			String refreshTokenCookie = ResponseCookie.from("refreshToken", tokenDto.getRefreshToken())
+				.path("/")
+				.sameSite("lax")
+				.build()
+				.toString();
+
+			response.addHeader("set-Cookie", accessTokenCookie);
+			response.addHeader("set-Cookie", refreshTokenCookie);
 		}
 
-		response.addCookie(accessTokenCookie);
-		response.addCookie(refreshTokenCookie);
 
 		response.sendRedirect(redirectUrl);
 	}
