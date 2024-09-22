b7f93e1b30b372d5cd8680119b0c3261b12b202e
rladydgn <39542757+rladydgn@users.noreply.github.com>
2024-09-22 22:34:43 +0900

    스웨거 https 요청 보내도록 수정

--- src/main/java/com/example/tinkerbell/TinkerbellApplication.java
+++ src/main/java/com/example/tinkerbell/TinkerbellApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @EnableJpaAuditing
 @SpringBootApplication
 public class TinkerbellApplication {
