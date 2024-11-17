fe526c4bc2759441e17d6a61dd5e070667dd0fd5
rladydgn <39542757+rladydgn@users.noreply.github.com>
2024-11-17 19:48:46 +0900

    order nullable 하게 수정

--- src/main/java/com/example/tinkerbell/todo/Dto/TodoDto.java
+++ src/main/java/com/example/tinkerbell/todo/Dto/TodoDto.java
@@ -1,7 +1,10 @@
 package com.example.tinkerbell.todo.Dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -46,5 +49,15 @@ public class TodoDto {
 		@JsonProperty("isCompleted")
 		private boolean isCompleted;
 		private LocalDateTime date;
+		private Integer order;
+	}
+
+	@Setter
+	@Getter
+	public static class Query {
+		@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+		private LocalDate from;
+		@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+		private LocalDate to;
 	}
 }
--- src/main/java/com/example/tinkerbell/todo/entity/Todo.java
+++ src/main/java/com/example/tinkerbell/todo/entity/Todo.java
@@ -43,5 +43,5 @@ public class Todo {
 	@Column
 	private LocalDateTime date;
 	@Column
-	private int order;
+	private Integer order;
 }
