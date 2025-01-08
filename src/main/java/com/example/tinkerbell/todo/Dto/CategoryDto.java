21ab599da6ac492bd93adeeae480b848929936bf
rladydgn <39542757+rladydgn@users.noreply.github.com>
2025-01-08 21:35:27 +0900

    카테고리 생성 요청 dto 네이밍 수정

--- src/main/java/com/example/tinkerbell/todo/Dto/CategoryDto.java
+++ src/main/java/com/example/tinkerbell/todo/Dto/CategoryDto.java
@@ -1,11 +1,13 @@
 package com.example.tinkerbell.todo.Dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 public class CategoryDto {
 	@Setter
 	@Getter
+	@Schema(name = "CategoryResponse")
 	public static class Request {
 		private String name;
 		private String color;
