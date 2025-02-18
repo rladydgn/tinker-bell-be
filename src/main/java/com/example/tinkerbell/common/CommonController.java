91f802617fb33d70dfd1d8731f2d911cc82622df
rladydgn <dhdgn@naver.com>
2025-02-19 02:16:03 +0900

    공휴일 api 명세 추가

--- src/main/java/com/example/tinkerbell/common/CommonController.java
+++ src/main/java/com/example/tinkerbell/common/CommonController.java
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 public class CommonController {
 	private final CommonService commonService;
 
-	@Operation(summary = "공휴일 조회 api", description = "연, 월을 파라미터로 입력하면 응답으로 공휴일인 날을 반환한다.")
+	@Operation(summary = "공휴일 조회 api", description = "연, 월을 파라미터로 입력하면 응답으로 공휴일인 날을 반환한다. 월은 10 미만일 경우 반드시 앞에 0을 붙여야 한다. ex. year: 2025, month: 01")
 	@GetMapping("holidays")
 	public ResponseEntity<List<OpenApiHolidayResponse>> getHolidays(HolidayQuery holidayQuery) {
 		return ResponseEntity.ok(commonService.getHolidays(holidayQuery));
